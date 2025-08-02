146e3dda9ec2cb9ea3eb4e9bec9e449bf7f4b5f3
HARISHMALISETTY <malisettyharish@gmail.com>
2025-08-02 09:03:54 +0530

    operators

---
+++ python/operators.py
@@ -0,0 +1,134 @@
+# a+b=sum
+
+# a='5'
+# b='10'
+# op=a+b
+# print(op)
+
+# - subtraction
+# * multiplication
+# %-modulus-->gives reminder as o/p 
+# /-division-->gives quotient as o/p. operation will perform until get remainder
+# //-floor division-->gives quotient (only integer part).
+# **-exponential-->used to get power of values
+# x=125
+# y=10
+# # print(x%y)
+# # print(x/y)
+# print(x//y)
+# a=4
+# b=5
+# print(a**b)
+
+# == and != -->to compare two values. whether they are equal or not
+# a=5
+# b='5'
+# print(a==b) #False
+# print('harish'=='Harish')
+# s_pswd=12345
+# i_pswd=12345
+# print(s_pswd==i_pswd)
+# print(a!=b) #True
+# print('harish'!='Harish') #True
+
+# # <,>,<=,>=
+
+
+# a=5, b='5', c='harish', d='Harish'
+
+# print(a==b or c==d) #False
+
+# print(a!=b or c!=d) #True
+
+# print(a==b or c!=d) #True
+
+# print (a!=b or c==d) # True
+
+# a=5
+# b=5
+# c=5
+# d=5
+
+# print(not(a==b and  c==d) )
+
+
+#assignment operatrs
+
+#= 
+# += addition assignment
+
+# x=10
+# x=x+5
+# print(x+=5)
+# print(x)
+# x-=2
+# x*=2
+# x/=2
+# x=25
+# x//=4
+# print(x)
+# x=4
+# x**=2 #x=x**2
+# print(x)
+
+# ip='hello'
+# search='k'
+# print(search in ip)
+
+# li1=['hi',45,789,'hello',98]
+# print('welcome' in li1)
+
+ipl_2026=['kohli','hardik','jadega','rohith','bumrah','MSD']
+player='MSD'
+print(player in ipl_2026)
+
+lcu=('kaidhi','vikram','leo','coolie')
+mve='coolie'
+
+print(mve in lcu)
+
+# batch_67r={'vamsi','ramakrishna','harsha','jayasree','ramyasree'}
+# stu='sai'
+# print(stu in batch_67r)
+
+# info={"stu1":"vamsi",
+#       "stud2":"ramakrishna",
+#       "stu3":"harsha",
+#       "stu4":"jayasree",
+#       "stu5":'ramyasree'}
+
+# info1={'stu1':"kiran",'stu2':'ramyasree','stu3':'vamsi'}
+
+# print('stu1' in info1)
+
+# a=350
+# b=350
+# print(id(a))
+# print(id(b))
+
+# print(a is b)
+
+
+li1=[4,7,8,9,10]
+
+li2=[6,7,8,9,12,45]
+
+#find the average of length of two lists
+
+# op=(len(li1)+len(li2))//2
+# print(op)
+
+# acnt_balance=25000
+# withdrwal=5000
+# # acnt_balnce?
+# acnt_balance-=withdrwal
+# slry=20000
+# acnt_balance+=slry
+# rent=7500
+# acnt_balance-=rent
+# print(acnt_balance)
+
+
+x="25"
+op=x*5
+print(op)
