43a85f6846e58b09ca5a454cec503ec2ed748e4b
HARISHMALISETTY <malisettyharish@gmail.com>
2025-08-18 08:44:22 +0530

    loops

---
+++ python/loops.py
@@ -0,0 +1,119 @@
+#looping statement:
+#set of statements will executes multiple times in a loop until a specific condition
+#reach.
+
+#to start this execution and end this execution we have to use
+#starting value and ending value. this can be achieve with help of
+#predefined method range().
+
+#range() will contains starting value(optional), ending value and stepper(optional).
+
+#for loop
+#while loop
+
+# for loop:
+# for x in range():
+#     #stat1
+#     #stat2
+#     #stat3
+
+# for x in range(1,10,3):
+    # print("hello world")
+    # print(x)
+
+# 2x1=2
+# 2x2=4
+# 2x3=6
+# .
+# .
+# .
+# 2x10=20
+
+# for x in range(1,11):
+#     print(f"2x{x}={2*x}")
+
+# print num from 20 to 50
+
+# for i in range(20,51):
+#     if(i%3==0):
+#        print(i)
+
+ #print num from 10 to 40
+ # if num is divisible by 3 then print num-fizz
+ # if num is divisible by 5 then print num-buzz
+ # if num is divisible by both then print num-fizzbuzz      
+
+# for y in range(10,41):
+#     if(y%3==0 and y%5==0):
+#         print(f"{y}-fizz buzz") 
+#     elif(y%5==0):
+#         print(f"{y}-buzz")
+#     elif(y%3==0):
+#         print(f"{y}-fizz") 
+
+        # 1 to 10
+        # 10 to 1
+# for x in range(10,0,-1):
+#     print(x)
+
+
+# str="fullstack"
+# for x in str:
+#     print(x)
+
+# li=['hi','hello','some','thing']
+# for x in li:
+#     print(x)
+
+# str="something"
+
+#s-hi
+#o-hi
+#m-hi
+# .
+# .
+# .
+# g-hi
+
+# for x in str:
+#     print(f"{x}-hi")
+
+# str="something"
+
+# for x in range(0,len(str),2):
+#     print(str[x])
+
+#1st-->x=0
+#2nd--->x=1
+
+# range-->single arg--->ending value 
+# range--->two args--->start and end 
+# range---->3 args--->start,end and stepper
+
+# str="something"
+# for x in range(len(str)-1,-1,-1):
+#     print(str[x])
+
+#hello
+#olleh
+
+ip="hello"
+#how to print
+# o 
+# l 
+# l 
+# e 
+# h
+
+#olleh
+op=""
+for x in range(len(ip)-1,-1,-1):
+    op+=ip[x]
+    print(op)
+
+#print numbers from 100 to 0 in reverse which are divisible by 5 
+#check whether given string is a palindrome or  not using a function
+#wirte a function to print tables in reverse
+# # for eg: 3x10=30
+#           3x9=27
+#           3x8=24    
--- python/statements.py
+++ python/statements.py
@@ -96,13 +96,65 @@
 #     #st3
 # else:
 #     #ds
-def college(eamcet_rank):
-    if (eamcet_rank<25000):
-        print("got seat in jntuh")
-    elif(eamcet_rank<50000):
-        print("got seat in mallareddy")
-    elif(eamcet_rank<75000):
-        print("got seat in sri indhu eng college")
-    else:
-        print("join in degree college")
-college(16500)
+# def college(eamcet_rank):
+#     if (eamcet_rank<25000):
+#         print("got seat in jntuh")
+#     elif(eamcet_rank<50000):
+#         print("got seat in mallareddy")
+#     elif(eamcet_rank<75000):
+#         print("got seat in sri indhu eng college")
+#     else:
+#         print("join in degree college")
+# college(16500)
+
+
+# terinary
+
+# nested-if :
+# if inside another if
+# won_last_match=True
+# total_matches_won=4
+# net_run_rate=0.78
+
+# if(won_last_match or total_matches_won>=6):
+#     if(total_matches_won>=5):
+#         if(net_run_rate>=0.75):
+#             print("team is entered into playoffs")
+#         else:
+#             print("team is disqualified due to low run rate")
+#     else:
+#         print("team should win more than 6 matches atleast to go to playoffs")
+# else:
+#     print("team lost in the current match, so not qualified")
+
+#terinary operator:
+#it is also known as conditional operator
+#it is used to write if-else in shorthand form in single line
+#with this we can assign the values to a variable conditionally.
+#we will use this only for simple and short requirement purpose only.
+#syntax---> True statement if True else False statement
+
+# print("true statement") if not True else print("false statement") 
+
+# username_matched=False
+# password_matched=True  
+# print("login success") if username_matched and password_matched else print("login failed")
+# print("login success") if username_matched and password_matched else print("login failed due to incorrect username") if not username_matched and password_matched else print("login failed due to incorrent password")
+
+
+# age=5
+# person="adult" if age>=18 else "minor"
+# print(person)
+
+
+login_as="owner"
+user="customer" if login_as=='customer'else 'owner'
+print(user)
+
+locality='local'
+shippingcost=5 if locality=='local' else 10
+print(shippingcost)
+
+marks=96
+grade='A' if marks>=95 else 'B'
+print(grade)
